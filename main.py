31e17ecf7be1baa24d3a35563ec13c59f001fb56
Amal Abdulkadir Osman <Amalabdulkadir67@gmail.com>
2026-01-12 20:42:22 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "d82efa08-3dba-489a-995c-67666657d162",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "",
+   "name": ""
+  },
+  "language_info": {
+   "name": ""
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
